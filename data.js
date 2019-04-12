180f965f6a4fdfd96dd0f991e718cd65ad869f90
Moriel Schottlender <moriel@gmail.com>
2019-04-12 11:43:14 +0300

    Reorganize huge data chunk into a separate file
    Google scripts allows for separate files that all load automatically,
    so we can utilize this to clean up the code a bit. This commit moves
    the huge images object block to a separate 'data' file and then
    loads it in the main 'code' through the variable.
    
    This requires us to open a new script file in Google Script editor
    ('File' -> 'New' -> 'Script file') and name it 'data', then paste
    the code from data.js into it.
    
    This separation of files can also help us make the code a bit more
    sustaining for the future, in case this experiment continues, without
    having to mess with a humongous block of text to fix something small
    or add more images, etc.

---
+++ data.js
@@ -0,0 +1,114 @@
+var Data = {
+	images: [
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Sleeping_cat_on_her_back.jpg/768px-Sleeping_cat_on_her_back.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Sleeping_cat_on_her_back.jpg',
+			attrib: 'Umberto Salvagnin [<a href="https://creativecommons.org/licenses/by/2.0">CC BY 2.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Sleeping_cat_on_her_back.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Six_weeks_old_cat_%28aka%29.jpg/640px-Six_weeks_old_cat_%28aka%29.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Six_weeks_old_cat_%28aka%29.jpg',
+			attrib: 'André Karwath aka Aka [<a href="https://creativecommons.org/licenses/by-sa/2.5">CC BY-SA 2.5</a>], <a href="https://commons.wikimedia.org/wiki/File:Six_weeks_old_cat_(aka).jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Tabby_cat_with_blue_eyes-3336579.jpg/499px-Tabby_cat_with_blue_eyes-3336579.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Tabby_cat_with_blue_eyes-3336579.jpg',
+			attrib: 'AdinaVoicu [CC0], <a href="https://commons.wikimedia.org/wiki/File:Tabby_cat_with_blue_eyes-3336579.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Siberian_black_tabby_blotched_cat_04.jpg/576px-Siberian_black_tabby_blotched_cat_04.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Siberian_black_tabby_blotched_cat_04.jpg',
+			attrib: 'Tadeáš Bednarz [<a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Siberian_black_tabby_blotched_cat_04.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Siberian_black_tabby_blotched_cat_01.jpg/512px-Siberian_black_tabby_blotched_cat_01.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Siberian_black_tabby_blotched_cat_01.jpg',
+			attrib: 'Tadeáš Bednarz [<a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Siberian_black_tabby_blotched_cat_01.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Baby_Goat_in_Margarita_Island%2C_Venezuela.jpg/640px-Baby_Goat_in_Margarita_Island%2C_Venezuela.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Baby_Goat_in_Margarita_Island,_Venezuela.jpg',
+			attrib: '<a href="/wiki/User:Beria" title="User:Beria">Béria L. Rodríguez</a> [<a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Baby_Goat_in_Margarita_Island,_Venezuela.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Baby_fur_seal%2C_South_Georgia.jpg/640px-Baby_fur_seal%2C_South_Georgia.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Baby_fur_seal,_South_Georgia.jpg',
+			attrib: 'Ville Miettinen from Helsinki, Finland [<a href="https://creativecommons.org/licenses/by/2.0">CC BY 2.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Baby_fur_seal,_South_Georgia.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Baby_Golden_Monkey_%2873313623%29.jpeg/640px-Baby_Golden_Monkey_%2873313623%29.jpeg',
+			url: 'https://commons.wikimedia.org/wiki/File:Baby_Golden_Monkey_(73313623).jpeg',
+			attrib: 'Blackseablue [<a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Baby_Golden_Monkey_(73313623).jpeg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Baby_Geese_01.jpg/640px-Baby_Geese_01.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Baby_Geese_01.jpg',
+			attrib: 'Cosmo1976 [<a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Baby_Geese_01.jpg">via Wikimedia Commons</a>',
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Baby_dwergnijlpaardje_Flory_%284022911547%29.jpg/640px-Baby_dwergnijlpaardje_Flory_%284022911547%29.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Baby_dwergnijlpaardje_Flory_(4022911547).jpg',
+			attrib: 'Sander van der Wel from Netherlands [<a href="https://creativecommons.org/licenses/by-sa/2.0">CC BY-SA 2.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Baby_dwergnijlpaardje_Flory_(4022911547).jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Baby_squirrel_%28orphaned_male%29_sleeping_in_human_hand.jpg/595px-Baby_squirrel_%28orphaned_male%29_sleeping_in_human_hand.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Baby_squirrel_(orphaned_male)_sleeping_in_human_hand.jpg',
+			attrib: 'Audrey from Central Pennsylvania, USA [<a href="https://creativecommons.org/licenses/by/2.0">CC BY 2.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Baby_squirrel_(orphaned_male)_sleeping_in_human_hand.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Bobcat-Texas-9135.jpg/576px-Bobcat-Texas-9135.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Bobcat-Texas-9135.jpg',
+			attrib: 'Loadmaster (David R. Tribble) This image was made by Loadmaster (David R. Tribble)Email the author:  David R. TribbleAlso see my personal gallery at Google Photos [<a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Bobcat-Texas-9135.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Happy_Kits%21_%287537056436%29.jpg/600px-Happy_Kits%21_%287537056436%29.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Happy_Kits!_(7537056436).jpg',
+			attrib: 'USFWS Mountain-Prairie [Public domain], <a href="https://commons.wikimedia.org/wiki/File:Happy_Kits!_(7537056436).jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Make_Room_for_Me%21_%287537056218%29.jpg/640px-Make_Room_for_Me%21_%287537056218%29.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Make_Room_for_Me!_(7537056218).jpg',
+			attrib: 'USFWS Mountain-Prairie [Public domain], <a href="https://commons.wikimedia.org/wiki/File:Make_Room_for_Me!_(7537056218).jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Turtle_mazunte_oaxaca_mexico_claudio_giovenzana_2010.jpg/640px-Turtle_mazunte_oaxaca_mexico_claudio_giovenzana_2010.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Turtle_mazunte_oaxaca_mexico_claudio_giovenzana_2010.jpg',
+			attrib: 'Giove [<a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Turtle_mazunte_oaxaca_mexico_claudio_giovenzana_2010.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Primitive_family_photo.jpg/640px-Primitive_family_photo.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Primitive_family_photo.jpg',
+			attrib: 'Parth.2211 [<a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Primitive_family_photo.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Phodopus_roborovskii_8_days_old_03.jpg/640px-Phodopus_roborovskii_8_days_old_03.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Phodopus_roborovskii_8_days_old_03.jpg',
+			attrib: 'Doronenko [<a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Phodopus_roborovskii_8_days_old_03.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Monkeys_yawn.jpg/640px-Monkeys_yawn.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Monkeys_yawn.jpg',
+			attrib: 'Parth.2211 [<a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Monkeys_yawn.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/HanRenne.JPG/512px-HanRenne.JPG',
+			url: 'https://commons.wikimedia.org/wiki/File:HanRenne.JPG',
+			attrib: 'GrottesdeHan [<a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>], <a href="https://commons.wikimedia.org/wiki/File:HanRenne.JPG">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Lulabi_%28257485715%29.jpeg/768px-Lulabi_%28257485715%29.jpeg',
+			url: 'https://commons.wikimedia.org/wiki/File:Lulabi_(257485715).jpeg',
+			attrib: 'Laura Von S [<a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Lulabi_(257485715).jpeg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Hoffmann%2C_Hans_-_A_Wild_Boar_Piglet_-_1578.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Hoffmann,_Hans_-_A_Wild_Boar_Piglet_-_1578.jpg',
+			attrib: 'Hans Hoffmann [Public domain], <a href="https://commons.wikimedia.org/wiki/File:Hoffmann,_Hans_-_A_Wild_Boar_Piglet_-_1578.jpg">via Wikimedia Commons</a>'
+		},
+		{
+			src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Rehkitz2.jpg/512px-Rehkitz2.jpg',
+			url: 'https://commons.wikimedia.org/wiki/File:Rehkitz2.jpg',
+			attrib: 'Dellex [<a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>], <a href="https://commons.wikimedia.org/wiki/File:Rehkitz2.jpg">via Wikimedia Commons</a>'
+		}
+	]
+};
