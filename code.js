f430500004b2ef57432842acb247bc9f284764de
Marko Obrovac <marko.obrovac@gmail.com>
2019-06-07 18:09:51 +0200

    Improve text and reason handling

--- code.js
+++ code.js
@@ -2,40 +2,43 @@
  * This code is the invoked by the trigger when a new form submission is made
  */
 function onNewData(e) {
-  if (!e || !e.namedValues) {
-    return;
-  }
-  var vals = e.namedValues;
-  var recipient = vals['Email of the person you want to send a thank you note to'][0];
-  var reasons = vals['What do you want to thank them for?'];
+	if (!e || !e.namedValues) {
+	  return;
+	}
+	var vals = e.namedValues;
+	var recipient = vals['What is the email of the person who you want to thank?'][0];
+	var reasons = vals['For what do you want to thank them?'][0].split( ', ' );
 
-  if ( Array.isArray( reasons ) ) {
-	  // Bold the reasons specifically
-	  reasons = reasons.map( function ( r ) {
-		  return '<strong>' + r + '</strong>';
-	  } );
+	if ( !recipient ) { return; }
 
-	  // Add "and" before the last one
-	  reasons[ reasons.length - 1 ] = 'and ' + reasons[ reasons.length - 1 ];
+	// Bold the reasons specifically
+	reasons = reasons.map( function ( r ) {
+		return '<strong>' + r + '</strong>';
+	} );
 
-	  // Join it all with commas
-	  reasons = reasons.join( ', ' );
-  }
+	// Add "and" before the last one
+	var lastReason = reasons.pop();
+	if ( reasons.length > 0 ) {
+		lastReason = ' and ' + lastReason;
+	}
 
-  var body = [
-	  'Hey you, worker bee!',
-	  '',
-	  'Somebody wants to show you are appreciated and are thanking you for: ' + reasons,
-	  '',
-	  'Here\'s a cute image, just for you:',
-	  getRandomCuteImage(),
-	  '',
-	  'Keep up the good work!',
-	  ''
-  ].join( '<br />' );
+	// Join it all with commas
+	reasons = reasons.join( ', ' ) + lastReason;
 
-  if ( !recipient ) { return; }
-  GmailApp.sendEmail(recipient, 'You have a thank you note', '', {htmlBody: body, noReply: true, name: 'Secret Admirer'});
+	var body = [
+		'Hey you, worker bee!',
+		'',
+		'A colleague wants to show you are appreciated, and in particular they are thanking you for: ' + reasons,
+		'',
+		'Here\'s a cute image, just for you:',
+		getRandomCuteImage(),
+		'',
+		'Keep up the good work!',
+		'', '',
+		'P.S. Want to thank someone yourself? Use <a href="https://forms.gle/keTMLhXnCfVjfnn8A">this form</a>'
+	].join( '<br />' );
+
+	GmailApp.sendEmail(recipient, 'You have a new staff thank you note', '', {htmlBody: body, noReply: true, name: 'Secret Admirer'});
 }
 
 /**
@@ -44,7 +47,7 @@ function onNewData(e) {
  * @return {string} Random image html tag
  */
 function getRandomCuteImage() {
-		img = getRandomItem( Data.images );
+	img = getRandomItem( Data.images );
 
 	return [
 		'<div style="text-align:center;">',
@@ -60,7 +63,7 @@ function getRandomCuteImage() {
  * Get a random object from within an array
  *
  * @private
- * @param  {string[]} arr Array of items
+ * @param {string[]} arr Array of items
  * @return {string} Random item
  */
 function getRandomItem( arr ) {
